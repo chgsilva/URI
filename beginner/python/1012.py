c5b92be2a0c03c778a433ae6614a1e683d1034a8
Carlos Silva <chgsilva@gmail.com>
2015-11-16 08:42:59 -0300

    a

--- beginner/python/1012.py
+++ beginner/python/1012.py
@@ -17,5 +17,5 @@ retangulo = a*b;
 print("TRIANGULO: %0.3f" % triangulo); 
 print("CIRCULO: %0.3f"   % circulo); 
 print("TRAPEZIO: %0.3f"  % trapezio); 
-print("QUADRADO: %0.3f"  % quadrado); 
+print("QUADRADO: %0.3f"  % quadrado);  
 print("RETANGULO: %0.3f" % retangulo);
